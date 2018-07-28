e9c9e733d4b0c1b6160ceb782cbcd4b4dde8e3ae
daniel <denial.torres@gmail.com>
2018-07-28 17:33:34 -0500

    add the twitter gem

--- app/models/keyword.rb
+++ app/models/keyword.rb
@@ -1,2 +1,14 @@
 class Keyword < ApplicationRecord
+  def grab_tweets    
+    client = Twitter::REST::Client.new do |config|
+      config.consumer_key        = "Npc5ideB1SzbwHn7492syZtVo"
+      config.consumer_secret     = "GkRjASfNwQilZvE373hfUSqQRIrAmU2nV6ZrG7zAMDZfyW8El9"
+      config.access_token        = "20212372-hhUf3F8LOnmfYCMrxbkWBAbs9lbMytek7ZR33i0M9"
+      config.access_token_secret = "SnsMgcgECBSiUG5C6kH7E10dKH4FQL0EDTbj4UzXPwH7n"
+    end
+    
+    client.search(self.word, result_type: "recent").take(3).collect do |tweet|
+      "#{tweet.user.screen_name}: #{tweet.text}"
+    end
+  end
 end

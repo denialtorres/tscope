1eb95ba972497a684d0350d069cb9ba2e67e4ccf
daniel <denial.torres@gmail.com>
2018-07-29 01:52:51 -0500

    datanase connection

--- config/initializers/database_connection.rb
+++ config/initializers/database_connection.rb
@@ -1,13 +1,10 @@
+
 #config/initializers/database_connection.rb
 Rails.application.config.after_initialize do
   ActiveRecord::Base.connection_pool.disconnect!
-
+ 
   ActiveSupport.on_load(:active_record) do
-    if Rails.application.config.database_configuration
-      config = Rails.application.config.database_configuration[Rails.env]
-      puts "HOOOOOLA"
-      puts config['reaping_frequency'] 
-      puts config['pool'] 
+    if config = ActiveRecord::Base.configurations[Rails.env] || Rails.application.config.database_configuration[Rails.env]
       config['reaping_frequency'] = ENV['DB_REAP_FREQ'] || 10 # seconds
       config['pool']              = ENV['DB_POOL']      || 5
       ActiveRecord::Base.establish_connection(config)

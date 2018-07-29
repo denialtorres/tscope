7fd8a2ec2c8b6cca040659b6525c6ad61146e2ef
daniel <denial.torres@gmail.com>
2018-07-29 01:49:59 -0500

    debuggin

--- config/initializers/database_connection.rb
+++ config/initializers/database_connection.rb
@@ -5,6 +5,7 @@ Rails.application.config.after_initialize do
   ActiveSupport.on_load(:active_record) do
     if Rails.application.config.database_configuration
       config = Rails.application.config.database_configuration[Rails.env]
+      binding.pry
       config['reaping_frequency'] = ENV['DB_REAP_FREQ'] || 10 # seconds
       config['pool']              = ENV['DB_POOL']      || 5
       ActiveRecord::Base.establish_connection(config)
